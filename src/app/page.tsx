7c8b9b39ff4de8cc55d0b420d104ab86b4efb391
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2025-02-17 15:52:29 +0300

    shadow text added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+"use client";
 import BentoGridMaker from "@/app/_components/bentomaker/bento-grid-maker";
 import { BackGround } from "@/app/_components/Background";
+import { LineShadowText } from "@/components/magicui/line-shadow-text";
+import { useTheme } from "next-themes";
 
 const page = () => {
+  const theme = useTheme();
+  const shadowColor = theme.resolvedTheme === "white" ? "white" : "white";
   return (
     <div className="min-h-screen px-4 py-16">
       <div className="max-w-4xl mx-auto">
         <BackGround />
-        <h1 className="text-4xl font-bold text-white mb-8 text-center">Bento Grid Maker</h1>
+        <h1 className="text-balance py-6 text-center text-white text-5xl font-semibold leading-none tracking-tighter sm:text-6xl md:text-8xl ">
+          Bento Grid {" "}
+          <LineShadowText className="" shadowColor={shadowColor}>
+             Maker 
+          </LineShadowText>
+        </h1>
         <BentoGridMaker />
       </div>
     </div>
